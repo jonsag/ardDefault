02717beb3461e358a4056ebfb5758961452bc6e0
jon <jonsagebrand@gmail.com>
2019-10-13 01:57:53 +0200

    added ardLCD sketch

---
+++ ardLCD/ardLCD.ino
@@ -0,0 +1,69 @@
+/*
+  The LCD circuit:
+                               GND            display pin 1
+    supply voltage for logic   +5V            display pin 2
+
+   10K potentiometer:
+   ends to +5V and ground
+   LCD VO - contrast           wiper          display pin 3
+
+   LCD RS pin                  digital pin 13 display pin 4
+   LCD R/W pin                 GND            display pin 5
+   LCD Enable pin              digital pin 12 display pin 6
+   LCD D4                      digital pin 11 display pin 11
+   LCD D5                      digital pin 10 display pin 12
+   LCD D6                      digital pin 9  display pin 13
+   LCD D7                      digital pin 8  display pin 14
+
+   220ohm resistor to +5V
+   back light anode:+4.2V      220ohm         display pin 15
+   back light cathode          GND	          display pin 16
+*/
+
+String programName = "ardLCD";
+String date = "201910013";
+String author = "Jon Sagebrand";
+String email = "jonsagebrand@gmail.com";
+
+/*******************************
+ * LCD setup
+ *******************************/
+// include the LCD library
+#include <LiquidCrystal.h>
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+// columns and rows of the LCD
+int lcdColumns = 16;
+int lcdRows = 2;
+
+void setup(void) {
+
+  // start LCD
+  lcd.begin(lcdColumns, lcdRows);
+  // print a message to the LCD
+  lcd.setCursor(0, 0);
+  lcd.print(programName);
+  lcd.setCursor(0, 1);
+  lcd.print("Booting ...");
+
+  // start serial port
+  lcd.setCursor(0, 1);
+  lcd.print("Starting serial ...");
+  Serial.begin(9600);
+
+  // print information
+  Serial.println(programName);
+  Serial.println(date);
+  Serial.print("by ");
+  Serial.print(author);
+  Serial.println(email);
+  Serial.println();
+
+  lcd.clear();
+}
+
+void loop(void) {
+	lcd.setCursor(0, 0);
+	lcd.print("Hello world");
+}
+}
