0026bb57e20e8cda0448c16dabe04b0c364cc388
jon <jon@core2duo>
2019-10-13 18:53:06 +0200

    ardlcd not working currently

--- ardLCD/ardLCD.ino
+++ ardLCD/ardLCD.ino
@@ -30,8 +30,27 @@ String email = "jonsagebrand@gmail.com";
  *******************************/
 // include the LCD library
 #include <LiquidCrystal.h>
+// set LCD pins
+const int LCD_RS = 13; // LCD RS, pin 4
+const int LCD_EN = 12; // LCD E, pin 6, Enable
+const int LCD_D4 = 11; // LCD D4, pin 11, databit 4
+const int LCD_D5 = 10; // LCD D5, pin 12, databit 5
+const int LCD_D6 = 9; // LCD D6, pin 13, databit 6
+const int LCD_D7 = 8; // LCD D7, pin 14, databit7
+/* other pins on LCD are:
+ * VSS, pin 1, GND
+ * VDD, pin 2, +5V
+ * V0, pin 3, 10k potentiometer, ends to +5V and GND, middle pin to V0, contrast
+ * R/W, pin 5, GND, Read/Write
+ * A, pin 15, 220ohm resistor, one end to +5V, other to A, back light anode, ~+4.2V
+ * K, pin 16, GND, back light cathode
+ * D0, pin 7, databit 0, not used/connected
+ * D1, pin 8, databit 1, not used/connected
+ * D2, pin 9, databit 2, not used/connected
+ * D3, pin 10, databit 3, not used/connected
+ */
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+LiquidCrystal lcd(&LCD_RS, &LCD_EN, &LCD_D4, &LCD_D5, &LCD_D6, &LCD_D7);
 // columns and rows of the LCD
 int lcdColumns = 16;
 int lcdRows = 2;
@@ -40,7 +59,7 @@ void setup(void) {
 
   // start LCD
   lcd.begin(lcdColumns, lcdRows);
-  // print a message to the LCD
+  // print name of this program and boot message to the LCD
   lcd.setCursor(0, 0);
   lcd.print(programName);
   lcd.setCursor(0, 1);
@@ -65,5 +84,7 @@ void setup(void) {
 void loop(void) {
 	lcd.setCursor(0, 0);
 	lcd.print("Hello world");
+	lcd.setCursor(0, 1);
+	lcd.print(millis() / 1000); // prints how many seconds this program has run
 }
 
